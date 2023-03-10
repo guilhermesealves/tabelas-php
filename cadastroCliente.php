22e8ff73a25b3460b2f9b146770d7ac0a372fd5e
Guilherme Seleri <110575996+guilhermesealves@users.noreply.github.com>
2023-03-09 21:47:51 -0300

    Atualizando

--- cadastroCliente.php
+++ cadastroCliente.php
@@ -6,11 +6,77 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <title>Document</title>
 </head>
+<style>
+    .tudo {
+        margin-top: 130px;
+        margin-left: 550px;
+        width: 500px;
+        height: 600px;
+        background-color: gainsboro;
+        padding-top: 30px;
+        padding-left: 45px;
+    }
+</style>
 
 <body>
+
+    <div class="tudo">
+    <div class="title">
+            <h1>Cadastro novo usuário</h1>
+        </div>
+
+        <div class="continer mt-5 ms-3">
+            <div class="col">
+                <div class="row">
+                    <div class="col-md-10 ms-">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <input type="text" name="cpf" class="form-control" placeholder="CPF">
+                            </div>
+                            <div class="form-group mt-1 ">
+                                <input type="text" name="nome" class="form-control" placeholder="Nome">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="email" name="email" class="form-control" placeholder="Bairro">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="number" name="phone" class="form-control" placeholder="Numero">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="number" name="phone" class="form-control" placeholder="Cidade">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="number" name="phone" class="form-control" placeholder="Estado">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="number" name="phone" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="number" name="phone" class="form-control" placeholder="Phone">
+                            </div>
+                            <div class="form-group mt-1">
+                                <input type="number" name="phone" class="form-control" placeholder="Whatsapp">
+                            </div>
+                            <div class="mt-3">
+                            </div>
+                            <p class="small mt-3">
+                                <button>sdsds</button>
+
+                            </p>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+
 
 
 
